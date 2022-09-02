7a1d4aae47b789d1b7d2c8b347272d343901c527
Tim Daubenschütz <tim@daubenschuetz.de>
2022-09-02 17:39:38 +0200

    Attempt to crawl soundxyz artists

--- src/soundxyz-filter-created-artist/step.mjs
+++ src/soundxyz-filter-created-artist/step.mjs
@@ -37,11 +37,33 @@ function decodeArtistAddress(log) {
 }
 
 const mapStep = (log) => ({ address: decodeArtistAddress(log) });
-export default (filePath = null) => [
-  {
-    name: "call-block-logs",
-    transformer: {
-      args: [filePath, filterStep, mapStep],
-    },
+const step0 = (filePath) => ({
+  name: "call-block-logs",
+  transformer: {
+    args: [filePath, filterStep, mapStep],
   },
-];
+});
+
+const transferEventSelector =
+  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+const emptyBytes32 =
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
+const filterStep1 = (log) =>
+  log.topics[0] === transferEventSelector &&
+  log.topics[1] === emptyBytes32 &&
+  Object.keys(contracts).includes(log.address);
+const mapStep1 = ({ metadata, log }) => ({
+  address: log.address,
+  tokenId: `${BigInt(log.topics[3]).toString(10)}`,
+  createdAtBlockNumber: `${parseInt(log.blockNumber, 16)}`,
+  platform: metadata.platform,
+});
+
+const step1 = {
+  name: "call-block-logs",
+  transformer: {
+    // NOTE: https://github.com/neume-network/strategies/issues/258
+    args: ["soundxyz-artists-created", filterStep1, mapStep1],
+  },
+};
+export default (filePath = null) => [step0(filePath), step1];
